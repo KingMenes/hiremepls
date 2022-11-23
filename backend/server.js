6fc9639ac9b8f2c3a7647239f9f52147a757a17c
BrenLau <blau4000@gmail.com>
2022-11-23 18:09:21 -0500

    first route working

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,9 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-//app.use("/api/whatever")
+import questions from "./api/questions_route.js"
+
+app.use("/api/questions", questions)
 app.use("*", (req, res) => res.status(404).json({ error: "not found" }))
 
 export default app
