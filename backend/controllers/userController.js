381eb4fa97a07d12afb2b95e959fdeed16691ccc
BrenLau <blau4000@gmail.com>
2022-11-24 18:26:29 -0500

    user create backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,9 @@
 import User from "../models/userModel.js"
 import asyncHandler from "express-async-handler"
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcryptjs'
 
 export const getUser = asyncHandler(async (req, res) => {
-    // const { questionsList } = await DBDAO.getQuestions({})
     const user = await User.findById(req.params.id)
     if (!user) {
         res.json({ error: "user not found" })
@@ -11,21 +12,57 @@ export const getUser = asyncHandler(async (req, res) => {
 })
 
 export const createUser = asyncHandler(async (req, res) => {
-    if (!req.body.question) {
+    const { username, email, password, role } = req.body
+    if (!username) {
         res.status(400)
-        throw new Error('Please add a question')
+        throw new Error('Please add a username')
     }
+    if (!password) {
+        res.status(400)
+        throw new Error('Please add a password')
+    }
+    if (!email) {
+        res.status(400)
+        throw new Error('Please add an email address')
+    }
+    const userExist = await User.findOne({ email })
+    if (userExist) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+    //password hashing
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
 
-    const question = await Question.create({
-        question: req.body.question,
-        author: req.body.author,
-        date: new Date(),
-        comments: [],
+    const user = await User.create({
+        username,
+        email,
+        password: hashedPassword,
+        role,
         reputation: [],
-        tags: req.body.tags,
     })
 
-    res.json(question)
+    if (user) {
+        res.status(201).json({ _id: user.id, username: user.username, email: user.email, reputation: user.reputation })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+})
+
+export const logInUser = asyncHandler(async (req, res) => {
+    const { email, password, id } = req.body
+    const user = await User.findOne(email)
+    if (!user) {
+        res.status(400)
+        throw new Error('User not found')
+    }
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.json({ _id: user.id, username: user.username, email: user.email, reputation: user.reputation })
+    } else {
+        res.status(400)
+        throw new Error('Invalid credentals')
+    }
 })
 
 export const updateUser = asyncHandler(async (req, res) => {
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 const router = express.Router();
 
-import { getUser, createUser, updateUser, deleteUser } from "../controllers/userController.js"
+import { getUser, createUser, updateUser, deleteUser, logInUser } from "../controllers/userController.js"
 
 router.get('/:id', getUser)
 router.post('/', createUser)
+router.post('/login', logInUser)
 router.put('/:id', updateUser)
 router.delete('/id', deleteUser)
 
