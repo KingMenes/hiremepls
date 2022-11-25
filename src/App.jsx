72aafa7fad031307f44b102e1f31bf65b53529dc
KingMenes <menaibr96@gmail.com>
2022-11-25 12:21:54 -0500

    renamed react components with .jsx extension added navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
-import SignUpForm from "./components/signupForm.js";
+import SignUpForm from "./components/SignupForm/SignupForm";
 
 function App() {
   return <div className="App"><SignUpForm></SignUpForm></div>;
