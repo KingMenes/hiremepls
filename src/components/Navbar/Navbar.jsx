207eb8a13c6b9279a3f522394986af00bf37c335
KingMenes <menaibr96@gmail.com>
2022-11-25 12:38:01 -0500

    added pages (such as home, contact, signup, etc)

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+function Navbar() {
+  return (
+      <div className="navbar">
+          <div>
+              hireme<span>pls</span>
+          </div>
+          <nav>
+              <ul>
+                  <li><a>Option 1</a></li>
+                  <li><a>Option 2</a></li>
+                  <li><a>Option 3</a></li>
+                  <li><a>Option 4</a></li>
+                  <li><a>Option 5</a></li>
+              </ul>
+          </nav>
+    </div>
+  )
+}
+
+export default Navbar
