248a82161bffdd6c5b4fa0859829e4a2e35b4019
BrenLau <blau4000@gmail.com>
2022-11-25 02:07:32 -0500

    made a signup form(basic, no functionality)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
+import SignUpForm from "./components/signupForm.js";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App"><SignUpForm></SignUpForm></div>;
 }
 
 export default App;
