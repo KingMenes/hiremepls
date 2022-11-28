d578f74e5c318cb126744656a2bad59cfdb2b15c
KingMenes <menaibr96@gmail.com>
2022-11-27 20:35:16 -0500

    started footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,69 @@
+import "./Footer.css";
+
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <footer>
+        <div className="products">
+          <span>Products</span>
+          <ul>
+            <li>
+              <a href="">Option 1</a>
+            </li>
+            <li>
+              <a href="">Option 2</a>
+            </li>
+            <li>
+              <a href="">Option 3</a>
+            </li>
+          </ul>
+        </div>
+        <div className="company">
+          <span>Company</span>
+          <ul>
+            <li>
+              <a href="">Option 1</a>
+            </li>
+            <li>
+              <a href="">Option 2</a>
+            </li>
+            <li>
+              <a href="">Option 3</a>
+            </li>
+          </ul>
+        </div>
+        <div className="support">
+          <span>Support</span>
+          <ul>
+            <li>
+              <a href="">Option 1</a>
+            </li>
+            <li>
+              <a href="">Option 2</a>
+            </li>
+            <li>
+              <a href="">Option 3</a>
+            </li>
+          </ul>
+        </div>
+        <div className="socials">
+          <span>Socials</span>
+          <ul>
+            <li>
+              <a href="">Facebook</a>
+            </li>
+            <li>
+              <a href="">Twitter</a>
+            </li>
+            <li>
+              <a href="">Instagram</a>
+            </li>
+          </ul>
+        </div>
+      </footer>
+      <span>© {new Date().getFullYear()} HireMePls. All rights reserved</span>
+    </div>
+  );
 }
 
 export default Footer;
