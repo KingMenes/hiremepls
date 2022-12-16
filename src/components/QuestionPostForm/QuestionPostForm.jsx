08bc793f0863aa56c7ed19317b148ff1c54ca101
KingMenes <menaibr96@gmail.com>
2022-12-16 02:30:16 -0500

    Added a styled question submission form

--- src/components/QuestionPostForm/QuestionPostForm.jsx
+++ src/components/QuestionPostForm/QuestionPostForm.jsx
@@ -1,6 +1,7 @@
 import './QuestionPostForm.css'
 import { useState } from "react";
 import { useDispatch } from "react-redux";
+import {motion} from 'framer-motion'
 
 
 
@@ -9,6 +10,8 @@ function QuestionPostForm() {
     title: '',
     body:''
   });
+
+  const [charCount,setCharCount] = useState('0')
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -36,34 +39,52 @@ function QuestionPostForm() {
     return (
       <div className="question-post">
         <div className='post-box'>
-        <h2>Ask a Question</h2>
-        <form onSubmit={onSubmit}>
-          <div className='post-title'>
-            <textarea
-              type="text"
-              placeholder="Ask a Question"
-              required
-              name="question-title"
-              onChange={onChange}
-              id="question-title"
-              value={formData.title}
-              maxLength="300"
-              rows="1"
-            ></textarea>
-            <div>0/300</div>
-          </div>
-          
-          <textarea
-            name="question-body"
-            id="question-body"
-            cols="30"
-            rows="10"
-            type="text"
-            placeholder="How would you answer? (optional)"
-            onChange={onChange}
-            value={formData.body}
-          ></textarea>
-        </form>
+          <h2>Ask a Question</h2>
+          <form onSubmit={onSubmit}>
+            <div className='post-title flex-center'>
+              <input
+                type="text"
+                placeholder="Question"
+                required
+                name="question-title"
+                onChange={onChange}
+                id="question-title"
+                // value={formData.title}
+                maxLength="300"
+              />
+              <div id='charcount'>0/300</div>
+            </div>
+            <div className='company-info flex-center'>
+              <input type="text" placeholder='For Position (optional)' />
+              <input type="text" placeholder='At Company Name (optional)' />
+            </div>
+            
+            <div className='post-body flex-center'>
+              <textarea
+                name="question-body"
+                id="question-body"
+                cols="30"
+                rows="10"
+                type="text"
+                placeholder="How would you answer? (optional)"
+                onChange={onChange}
+                // value={formData.body}
+                ></textarea>
+            </div>
+            <div className='add-tag flex-center'>
+              <h3>Include tags to help others find your question!</h3>
+              <input type="text" placeholder='Add a tag here (max 5)' />
+              <p>ex: engineering, human resources, software, technical</p>
+            </div>
+            <div className='tags'></div>
+            <div className='btn-div'>
+              <motion.button type='submit'
+              whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}>
+                Submit
+              </motion.button>
+            </div>
+          </form>
         </div>
       </div>
     )
