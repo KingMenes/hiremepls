f04b47e63887b6b6b3cf460ed5faad224ea0a697
Mena <menaibr96@gmail.com>
2022-12-17 03:53:36 -0500

    removed CreateQuestion from home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,9 @@
 import Header from "../components/Header/Header";
 import SectionCardContainer from "../components/SectionCardContainer/SectionCardContainer";
-import CreateQuestion from "../components/CreateQuestion";
 
 function Home() {
   return (
     <>
-      <CreateQuestion />
       <Header />
       <SectionCardContainer />
     </>
