930b8698c0a1b4bb4a6ae50dac4aaa2204608467
Mena <menaibr96@gmail.com>
2022-12-17 03:52:45 -0500

    Form css finished, still need to add functionality

--- src/components/QuestionPostForm/QuestionPostForm.jsx
+++ src/components/QuestionPostForm/QuestionPostForm.jsx
@@ -1,15 +1,35 @@
 import "./QuestionPostForm.css";
-import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import { createQuestion } from "../../store/questions";
 import { motion } from "framer-motion";
+import { AiOutlinePlus , AiOutlineClose} from 'react-icons/ai'
 
 function QuestionPostForm() {
+
+  const dispatch = useDispatch();
+  const sessionUser = useSelector((state) => state.session.user);
+  const [question, setQuestion] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await dispatch(createQuestion({ question, user: sessionUser })); //Object to POST
+  };
+
   const [formData, setFormData] = useState({
     title: "",
     body: "",
   });
 
+  const tags = ['tag1', 'tag2', 'tag3', 'tag4', 'tag5']
+
   const [charCount, setCharCount] = useState("0");
+  // const [tags, setTags] = useState([])
+
+  // function updateTags(e) {
+  //   setTags(tags => [...tags, e.target.value])
+  //   e.target.value = ''
+  //   console.log(tags)
+  // }
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -18,26 +38,12 @@ function QuestionPostForm() {
     }));
   };
 
-  const onSubmit = async (e) => {
-    // e.preventDefault();
-    // const userData = {
-    //   username,
-    //   email,
-    //   password,
-    //   confirmPassword: password2,
-    // };
-    // const res = await dispatch(registerUser(userData));
-    // if (res?.payload?.username === userData.username) {
-    //   setSessionUser(res.payload);
-    //   handleClose();
-    // }
-  };
 
   return (
     <div className="question-post">
       <div className="post-box">
         <h2>Ask a Question</h2>
-        <form onSubmit={onSubmit}>
+        <form onSubmit={handleSubmit}>
           <div className="post-title flex-center">
             <input
               type="text"
@@ -55,7 +61,6 @@ function QuestionPostForm() {
             <input type="text" placeholder="For Position (optional)" />
             <input type="text" placeholder="At Company Name (optional)" />
           </div>
-
           <div className="post-body flex-center">
             <textarea
               name="question-body"
@@ -70,10 +75,22 @@ function QuestionPostForm() {
           </div>
           <div className="add-tag flex-center">
             <h3>Include tags to help others find your question!</h3>
-            <input type="text" placeholder="Add a tag here (max 5)" />
-            <p>ex: engineering, human resources, software, technical</p>
+            <div>
+              <AiOutlinePlus className="icn" />
+              <input type="text" placeholder="Add a tag here (max 5)"
+                // onSubmit={updateTags}
+              />
+            </div>
+            <p>ex: general, human resources, software, technical</p>
           </div>
-          <div className="tags"></div>
+          <div className="tags">
+            {tags.map((tag) => (
+              <div className="tag" key={tag}>
+                <span>{tag}</span>
+                <AiOutlineClose className="icn"/>
+              </div>
+            ))}
+          </div>
           <div className="btn-div">
             <motion.button
               type="submit"
