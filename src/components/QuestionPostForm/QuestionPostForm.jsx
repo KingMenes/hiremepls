ff63f04147afd281726854e76999ad4b447aa153
BrenLau <blau4000@gmail.com>
2022-12-19 23:21:02 -0500

    modal works for questions

--- src/components/QuestionPostForm/QuestionPostForm.jsx
+++ src/components/QuestionPostForm/QuestionPostForm.jsx
@@ -4,8 +4,10 @@ import { useState, useEffect } from "react";
 import { createQuestion } from "../../store/questions";
 import { motion } from "framer-motion";
 import { AiOutlinePlus, AiOutlineClose } from 'react-icons/ai'
+import Backdrop from "../Backdrop/Backdrop";
 
-function QuestionPostForm() {
+
+function QuestionPostForm({ handleClose }) {
 
   const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
@@ -46,70 +48,73 @@ function QuestionPostForm() {
 
 
   return (
-    <div className="question-post">
-      <div className="post-box">
-        <h2>Ask a Question</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="post-title flex-center">
-            {errors ? <div>{errors}</div> : null}
-            <input
-              type="text"
-              placeholder="Question"
-              required
-              name="question-title"
-              onChange={onChange}
-              id="question-title"
-              // value={formData.title}
-              maxLength="300"
-            />
-            <div id="charcount">0/300</div>
-          </div>
-          <div className="company-info flex-center">
-            <input type="text" placeholder="For Position (optional)" />
-            <input type="text" placeholder="At Company Name (optional)" />
-          </div>
-          <div className="post-body flex-center">
-            <textarea
-              name="question-body"
-              id="question-body"
-              cols="30"
-              rows="10"
-              type="text"
-              placeholder="How would you answer? (optional)"
-              onChange={onChange}
-            // value={formData.body}
-            ></textarea>
-          </div>
-          <div className="add-tag flex-center">
-            <h3>Include tags to help others find your question!</h3>
-            <div>
-              <AiOutlinePlus className="icn" />
-              <input type="text" placeholder="Add a tag here (max 5)"
-              // onSubmit={updateTags}
+    <Backdrop onClick={handleClose}>
+
+      <div className="question-post">
+        <div className="post-box">
+          <h2>Ask a Question</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="post-title flex-center">
+              {errors ? <div>{errors}</div> : null}
+              <input
+                type="text"
+                placeholder="Question"
+                required
+                name="question-title"
+                onChange={onChange}
+                id="question-title"
+                // value={formData.title}
+                maxLength="300"
               />
+              <div id="charcount">0/300</div>
             </div>
-            <p>ex: general, human resources, software, technical</p>
-          </div>
-          <div className="tags">
-            {tags.map((tag) => (
-              <div className="tag" key={tag}>
-                <span>{tag}</span>
-                <AiOutlineClose className="icn" />
+            <div className="company-info flex-center">
+              <input type="text" placeholder="For Position (optional)" />
+              <input type="text" placeholder="At Company Name (optional)" />
+            </div>
+            <div className="post-body flex-center">
+              <textarea
+                name="question-body"
+                id="question-body"
+                cols="30"
+                rows="10"
+                type="text"
+                placeholder="How would you answer? (optional)"
+                onChange={onChange}
+              // value={formData.body}
+              ></textarea>
+            </div>
+            <div className="add-tag flex-center">
+              <h3>Include tags to help others find your question!</h3>
+              <div>
+                <AiOutlinePlus className="icn" />
+                <input type="text" placeholder="Add a tag here (max 5)"
+                // onSubmit={updateTags}
+                />
               </div>
-            ))}
-          </div>
-          <div className="btn-div">
-            <motion.button
-              type="submit"
-              whileHover={{ scale: 1.1 }}
-              whileTap={{ scale: 0.9 }}
-            >
-              Submit
-            </motion.button>
-          </div>
-        </form>
+              <p>ex: general, human resources, software, technical</p>
+            </div>
+            <div className="tags">
+              {tags.map((tag) => (
+                <div className="tag" key={tag}>
+                  <span>{tag}</span>
+                  <AiOutlineClose className="icn" />
+                </div>
+              ))}
+            </div>
+            <div className="btn-div">
+              <motion.button
+                type="submit"
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+              >
+                Submit
+              </motion.button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </Backdrop>
   );
 }
 
