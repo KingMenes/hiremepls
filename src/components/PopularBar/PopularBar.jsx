551528d7ec89458b5d55a2c76ee632382c905a28
KingMenes <menaibr96@gmail.com>
2022-12-30 15:45:08 -0500

    Commented out questions sorts cuz it broke something

--- src/components/PopularBar/PopularBar.jsx
+++ src/components/PopularBar/PopularBar.jsx
@@ -6,35 +6,35 @@ import { useSelector } from "react-redux";
 
 function PopularBar() {
 
-  const questions = useSelector((state) => state.questions);
-  function calculateHotness(question) {
-    const votes = question.reputation[0] - question.reputation[1];
-    const date = question.date;
+  // const questions = useSelector((state) => state.questions);
+  // function calculateHotness(question) {
+  //   const votes = question.reputation[0] - question.reputation[1];
+  //   const date = question.date;
   
-    // calculate the difference in seconds between now and the post date
-    const secondsSincePost = (new Date() - date) / 1000;
+  //   // calculate the difference in seconds between now and the post date
+  //   const secondsSincePost = (new Date() - date) / 1000;
   
-    // use the Reddit hotness formula to calculate the hotness score
-    const hotness = votes / Math.pow(secondsSincePost + 2, 1.5);
+  //   // use the Reddit hotness formula to calculate the hotness score
+  //   const hotness = votes / Math.pow(secondsSincePost + 2, 1.5);
   
-    return hotness;
-  }
+  //   return hotness;
+  // }
 
-  const sortedByHot = questions.sort((a, b) => calculateHotness(b) - calculateHotness(a));
-  const sortedByNew = questions.sort((a, b) => {
+  // const sortedByHot = questions.sort((a, b) => calculateHotness(b) - calculateHotness(a));
+  // const sortedByNew = questions.sort((a, b) => {
 
-    // Convert the date strings to Date objects
-    const dateA = new Date(a.date);
-    const dateB = new Date(b.date);
+  //   // Convert the date strings to Date objects
+  //   const dateA = new Date(a.date);
+  //   const dateB = new Date(b.date);
 
-    // Sort in ascending order
-    if (dateA > dateB) return -1;
-    if (dateA < dateB) return 1;
+  //   // Sort in ascending order
+  //   if (dateA > dateB) return -1;
+  //   if (dateA < dateB) return 1;
 
-    // If dates are equal
-    return 0;
-  });
-  const sortedByTop = questions.slice().sort((a, b) => (b.reputation[0]- b.reputation[1]) - (a.reputation[0]- a.reputation[1]));
+  //   // If dates are equal
+  //   return 0;
+  // });
+  // const sortedByTop = questions.slice().sort((a, b) => (b.reputation[0]- b.reputation[1]) - (a.reputation[0]- a.reputation[1]));
 
   return (
     <div className="popularbar">
