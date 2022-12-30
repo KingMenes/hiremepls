5c6f882fa84221244a31e74c85bad8c24ae54758
KingMenes <menaibr96@gmail.com>
2022-12-30 15:38:53 -0500

    Added sorting functions to the PopularBar component

--- src/components/PopularBar/PopularBar.jsx
+++ src/components/PopularBar/PopularBar.jsx
@@ -4,6 +4,36 @@ import { TiStarburstOutline } from "react-icons/ti";
 import { BsBarChart } from "react-icons/bs";
 
 function PopularBar() {
+
+  function calculateHotness(question) {
+    const votes = question.reputation[0] - question.reputation[1];
+    const date = question.date;
+  
+    // calculate the difference in seconds between now and the post date
+    const secondsSincePost = (new Date() - date) / 1000;
+  
+    // use the Reddit hotness formula to calculate the hotness score
+    const hotness = votes / Math.pow(secondsSincePost + 2, 1.5);
+  
+    return hotness;
+  }
+
+  const sortedByHot = questions.sort((a, b) => calculateHotness(b) - calculateHotness(a));
+  const sortedByNew = questions.sort((a, b) => {
+
+    // Convert the date strings to Date objects
+    const dateA = new Date(a.date);
+    const dateB = new Date(b.date);
+
+    // Sort in ascending order
+    if (dateA > dateB) return -1;
+    if (dateA < dateB) return 1;
+
+    // If dates are equal
+    return 0;
+  });
+  const sortedByTop = questions.slice().sort((a, b) => (b.reputation[0]- b.reputation[1]) - (a.reputation[0]- a.reputation[1]));
+
   return (
     <div className="popularbar">
       <ul>
