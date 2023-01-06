3a4a2e7aff62075f3e4d18b5765541ed68ada11e
BrenLau <blau4000@gmail.com>
2023-01-06 00:09:52 -0500

    process env endpoints for devlopment vs production

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+const API_ENDPOINT = process.env.REACT_APP_SERVER_URL || '/api';
 
 export default axios.create({
-  baseURL: "https://hiremepls-api.onrender.com/api",
+  baseURL: API_ENDPOINT,
   headers: {
     "Content-type": "application/json",
   },
