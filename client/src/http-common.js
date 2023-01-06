9a7bb776d95aea7be3c94dbb6d0fca63072e162a
BrenLau <blau4000@gmail.com>
2023-01-06 00:21:18 -0500

    again

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_ENDPOINT = process.env.REACT_APP_SERVER_URL || '/api';
+const API_ENDPOINT = process.env.environment === 'development' ? `${process.env.REACT_APP_SERVER_URL}/api` : "http://localhost:5000/api";
 
 export default axios.create({
   baseURL: API_ENDPOINT,
