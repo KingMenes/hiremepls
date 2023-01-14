7f0dd4d623dfbad3e20daf448402c406bb51e6c8
BrenLau <blau4000@gmail.com>
2023-01-14 15:53:56 -0500

    removed unused code from session store

--- client/src/store/session.js
+++ client/src/store/session.js
@@ -5,7 +5,6 @@ import http from "../http-common";
 // constants
 const SET_USER = "session/SET_USER";
 const REMOVE_USER = "session/REMOVE_USER";
-const CHECK_USER = "sesesefgsefsegfsda";
 
 export const login = createAsyncThunk(
   SET_USER,
@@ -64,86 +63,3 @@ const sessionSlice = createSlice({
 
 const { reducer } = sessionSlice;
 export default reducer;
-
-// ACTIONS 👇
-
-const setUser = (user) => ({
-  type: SET_USER,
-  payload: user,
-});
-
-const removeUser = () => ({
-  type: REMOVE_USER,
-});
-
-const checkUser = (user) => ({
-  type: CHECK_USER,
-  user,
-});
-
-export const checkUserThunk = (userId) => async (dispatch) => {
-  const res = await fetch(`/api/users/${userId}`);
-
-  if (res.ok) {
-    const data = await res.json();
-    await dispatch(checkUser(data));
-    return data;
-  }
-};
-const initialState = { user: null };
-
-export const authenticate = () => async (dispatch) => {
-  const response = await fetch("/api/auth/", {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  if (response.ok) {
-    const data = await response.json();
-    if (data.errors) {
-      return;
-    }
-
-    dispatch(setUser(data));
-  }
-};
-
-export const signUp = (username, email, password) => async (dispatch) => {
-  const response = await fetch("/api/auth/signup", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      username,
-      email,
-      password,
-    }),
-  });
-
-  if (response.ok) {
-    const data = await response.json();
-    dispatch(setUser(data));
-    return data;
-  } else if (response.status < 500) {
-    const data = await response.json();
-    if (data.errors) {
-      return data;
-    }
-  } else {
-    return ["An error occurred. Please try again."];
-  }
-};
-
-// export default function reducer(state = initialState, action) {
-//     switch (action.type) {
-//         case SET_USER:
-//             return { user: action.payload }
-//         case REMOVE_USER:
-//             return { user: null }
-
-//         default:
-//             return state;
-//     }
-// }
-
