b738d395d72f369ae51fa2a2e5e7eef9a8a27223
BrenLau <blau4000@gmail.com>
2023-01-24 12:40:44 -0500

    adjusted sessio auth

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -2,12 +2,6 @@ import axios from "axios";
 
 const API_ENDPOINT = process.env.REACT_APP_SERVER_URL || '/api';
 
-const env = process.env.NODE_ENV || "development";
-const API_ENDPOINT =
-  env === "production"
-    ? "http://localhost:5000/"
-    : "https://hiremepls-api.onrender.com/";
-
 export default axios.create({
   baseURL: API_ENDPOINT,
   headers: {
