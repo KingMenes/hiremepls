b1d567673b142c73f8f3c9eed84c5189cea4c912
BrenLau <blau4000@gmail.com>
2023-03-14 16:52:29 -0400

    removed dupes

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,6 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 import cookieParser from "cookie-parser";
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
