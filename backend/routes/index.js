bf6ca555143a346877dcdae8388546be07e5dded
BrenLau <blau4000@gmail.com>
2023-03-14 21:34:02 -0400

    fds

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,21 +2,17 @@
 import express from 'express'
 const router = express.Router();
 import path from 'path'
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 // Static routes
 // Serve React build files in production
 if (process.env.NODE_ENV === "production") {
   // Serve the frontend's index.html file at the root route
   router.get("/", (req, res) => {
     res.sendFile(
-      path.resolve(__dirname, "../../client", "build", "index.html")
+      path.resolve(__dirname, "../../frontend", "build", "index.html")
     );
   });
   // Serve the static assets in the frontend's build folder
-  router.use(express.static(path.resolve("../frontend/client")));
+  router.use(express.static(path.resolve("../client/build")));
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.sendFile(
