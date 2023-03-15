28c354edee2548a90aed109e7596f795c53aafc9
BrenLau <blau4000@gmail.com>
2023-03-14 21:09:17 -0400

    fds

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,7 +7,6 @@ import path from 'path'
 if (process.env.NODE_ENV === "production") {
   // Serve the frontend's index.html file at the root route
   router.get("/", (req, res) => {
-    res.cookie("XSRF-TOKEN", req.csrfToken());
     res.sendFile(
       path.resolve(__dirname, "../../client", "build", "index.html")
     );
@@ -16,7 +15,6 @@ if (process.env.NODE_ENV === "production") {
   router.use(express.static(path.resolve("../frontend/client")));
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
-    res.cookie("XSRF-TOKEN", req.csrfToken());
     res.sendFile(
       path.resolve(__dirname, "../../client", "build", "index.html")
     );
@@ -25,7 +23,6 @@ if (process.env.NODE_ENV === "production") {
 // Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== "production") {
   router.get("/api/csrf/restore", (req, res) => {
-    res.cookie("XSRF-TOKEN", req.csrfToken());
     res.status(201).json({});
   });
 }
