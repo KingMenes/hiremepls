58d1fe94b8e997ee9ca1c581d38dcc96362f439f
BrenLau <blau4000@gmail.com>
2023-03-14 21:35:15 -0400

    fds

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,6 +2,10 @@
 import express from 'express'
 const router = express.Router();
 import path from 'path'
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 // Static routes
 // Serve React build files in production
 if (process.env.NODE_ENV === "production") {
